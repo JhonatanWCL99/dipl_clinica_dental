c725dc336d05ff393e67b01142c46e8769d64cb7
JhonatanWCL99 <wilsoncoyoleon@gmail.com>
2023-04-27 22:22:20 -0400

    sistema para clinica dental

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USUARIO', 'root');
+define('DB_PASSWORD', '');
+define('DB_NOMBRE', 'examen_final');
+
+//Ruta de la App
+define("RUTA_APP", dirname(dirname(__FILE__)));
+
+
+
+define('RUTA_URL', 'http://localhost/ExamenFinal');
+
+define('NOMBRE_SITIO', 'ExamenFinal');
+
+define('VIEWS', '../app/views/');
+define('CONTROLLERS', '../app/controllers/');
+define('MODELS', '../app/models/');
